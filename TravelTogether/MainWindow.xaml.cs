3fe14ec4db3706403668ed967ddb0c34f7d89b7a
Mystic <mysteriousGE@gmail.com>
2017-10-04 21:10:11 +0800

    Initial Version

---
+++ TravelTogether/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+
+namespace TravelTogether
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Add_Click(object sender, RoutedEventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                var result = fbd.ShowDialog();
+                if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    //string[] files = Directory.GetFiles();
+                    //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                    listBox_Folder.Items.Add(fbd.SelectedPath);
+                }
+            }
+        }
+    }
+}
